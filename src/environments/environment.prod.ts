3d1dc80989cabb22a6f102f63be286107a4799ee
DmytroLavrov <dimalavrov2004.06.16@gmail.com>
2026-02-17 14:32:16 +0200

    chore: initial project setup

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    projectId: 'mydim-osbb',
+    appId: '1:818328673222:web:7a8202f839457f3789f0c7',
+    storageBucket: 'mydim-osbb.firebasestorage.app',
+    apiKey: 'AIzaSyCdL7dr5MlLNBb6DUDXmlsaycMWx6zs1no',
+    authDomain: 'mydim-osbb.firebaseapp.com',
+    messagingSenderId: '818328673222',
+  },
+};
