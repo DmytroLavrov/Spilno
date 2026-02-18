64e74fe3e23e6a89aff595ffa298f3d38c9c7c94
DmytroLavrov <dimalavrov2004.06.16@gmail.com>
2026-02-18 20:09:25 +0200

    feat(styles): add PrimeIcons stylesheet and configure PrimeNG theme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,18 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '@env/environment';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
