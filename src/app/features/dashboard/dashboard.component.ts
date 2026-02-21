72ef4cada2546e757558b28abe099958878e569e
DmytroLavrov <dimalavrov2004.06.16@gmail.com>
2026-02-21 11:00:03 +0200

    feat(dashboard): add DashboardComponent with role-based rendering

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,12 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { AuthService } from '@core/services/auth.service';
+import { AdminDashboardComponent } from '@features/dashboard/admin-dashboard/admin-dashboard.component';
+import { ResidentDashboardComponent } from '@features/dashboard/resident-dashboard/resident-dashboard.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [AdminDashboardComponent, ResidentDashboardComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardComponent {
-
+  public authService = inject(AuthService);
 }
