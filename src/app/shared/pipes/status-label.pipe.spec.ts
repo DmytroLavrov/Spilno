44085e3b5fbe866ff02ce7070f86bf58ebeb865d
DmytroLavrov <dimalavrov2004.06.16@gmail.com>
2026-03-22 11:50:42 +0200

    feat(tests): enhance status label pipe tests with request and user status translations

--- src/app/shared/pipes/status-label.pipe.spec.ts
+++ src/app/shared/pipes/status-label.pipe.spec.ts
@@ -1,8 +1,33 @@
-import { StatusLabelPipe } from './status-label.pipe';
+import { RequestStatus } from '@models/request.model';
+import { StatusLabelPipe } from '@shared/pipes/status-label.pipe';
+import { UserStatus } from '@models/user.model';
 
 describe('StatusLabelPipe', () => {
+  let pipe: StatusLabelPipe;
+
+  beforeEach(() => {
+    pipe = new StatusLabelPipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new StatusLabelPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should correctly translate request statuses (RequestStatus)', () => {
+    expect(pipe.transform('new' as RequestStatus)).toBe('Нова');
+    expect(pipe.transform('in_progress' as RequestStatus)).toBe('В обробці');
+    expect(pipe.transform('done' as RequestStatus)).toBe('Виконано');
+    expect(pipe.transform('rejected' as RequestStatus)).toBe('Відхилено');
+  });
+
+  it('should correctly translate user statuses (UserStatus)', () => {
+    expect(pipe.transform('pending' as UserStatus)).toBe('Очікує');
+    expect(pipe.transform('active' as RequestStatus)).toBe('Активний');
+    expect(pipe.transform('rejected' as RequestStatus)).toBe('Відхилено');
+  });
+
+  it('should return the original value if the status is unknown', () => {
+    const unknownStatus = 'some_weird_status';
+    expect(pipe.transform(unknownStatus as any)).toBe(unknownStatus);
   });
 });
